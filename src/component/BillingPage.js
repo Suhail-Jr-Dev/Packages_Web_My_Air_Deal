79ae1cff8bb41761416f160832ab78b346e87938
Suhail-sJrD <suhailsince2003@gmail.com>
2024-10-07 17:49:28 +0530

    adding package json data

--- src/component/BillingPage.js
+++ src/component/BillingPage.js
@@ -251,7 +251,7 @@ function BillingPage({ temp, setTemp }) {
                                 <div className="relative z-0 w-full mb-5 group">
                                     <input type="tel" name="passengers" id="passengers" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required value={passengers}
                                         onChange={(e) => setPassengers(e.target.value)} />
-                                    <label htmlFor="passengers" className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0">passengers</label>
+                                    <label htmlFor="passengers" className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0">Passengers</label>
                                 </div>
                             </div>
 
